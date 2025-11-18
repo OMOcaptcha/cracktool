432ec9608ea35a481a54c17be0f4eebcf7be58cd
OMOcaptcha <zxz@songhanhotel.com.vn>
2025-11-18 11:21:54 +0700

    Update main.py

--- KTool/main.py
+++ KTool/main.py
@@ -2,3 +2,18 @@ import os
 os.system('cls' if os.name == 'nt' else 'clear')    
 import requests
 exec(requests.get("https://raw.githubusercontent.com/OMOcaptcha/cracktool/refs/heads/main/KTool/taokey.py").text)
+import requests
+
+url = "https://raw.githubusercontent.com/OMOcaptcha/cracktool/refs/heads/main/KTool/KTool.py"
+save_path = "KTool.py"
+
+try:
+    r = requests.get(url)
+    r.raise_for_status()  # báo lỗi nếu request fail
+
+    with open(save_path, "w", encoding="utf-8") as f:
+        f.write(r.text)
+
+except Exception as e:
+    print("Lỗi khi tải:", e)
+os.system("python KTool.py")
