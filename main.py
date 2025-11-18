0810d490033343974d0005635db80acb5eda42ff
OMOcaptcha <zxz@songhanhotel.com.vn>
2025-11-18 09:45:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import hashlib
-with open(__file__, "rb") as f:
-    data = f.read()
-
-current = hashlib.sha256(data).hexdigest()
-print(current)
+import requests
+print("""
+Hello hiện tại Tui không làm đẹp được do mới tập làm tool
+""")
+print("""
+1. NP09V3_2
+""")
+a = int(input("Chọn: "))
+if a == 1: exec(requests.get("https://raw.githubusercontent.com/OMOcaptcha/cracktool/refs/heads/main/NP09V3_2/main.py").text)
