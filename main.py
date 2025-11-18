7bba15c68f659ceb190ac6cfd7a4bcb5bb48a174
OMOcaptcha <zxz@songhanhotel.com.vn>
2025-11-18 09:47:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import hashlib
+with open(__file__, "rb") as f:
+    data = f.read()
+
+current = hashlib.sha256(data).hexdigest()
+print(current)
 import requests
 print("""
 Hello hiện tại Tui không làm đẹp được do mới tập làm tool
