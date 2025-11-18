32570baae3d80e65f72919e803cea74140515a69
OMOcaptcha <zxz@songhanhotel.com.vn>
2025-11-18 10:24:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,4 @@ print("""
 """)
 a = int(input("Chọn: "))
 if a == 1: exec(requests.get("https://raw.githubusercontent.com/OMOcaptcha/cracktool/refs/heads/main/NP09V3_2/main.py").text)
+if a == 2: exec(requests.get("https://raw.githubusercontent.com/OMOcaptcha/cracktool/refs/heads/main/KTool/main.py").text)
