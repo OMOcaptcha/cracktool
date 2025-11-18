bee088a0dfee1db63cac4644583373a3734092dc
OMOcaptcha <zxz@songhanhotel.com.vn>
2025-11-18 11:01:19 +0700

    Create main.py

---
+++ KTool/main.py
@@ -0,0 +1,13 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')    
+import requests
+print("""
+KTool crack
+""")
+print("""
+1. KTool skibidi (crack lỏ)
+2. KTool (full menu crack đc)
+""")
+a = int(input("Chọn: "))
+if a == 1: exec(requests.get("https://raw.githubusercontent.com/OMOcaptcha/cracktool/refs/heads/main/KTool/mainn.py").text)
+if a == 2: print("đang chưa xong")
