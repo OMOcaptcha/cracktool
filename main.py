9d8d18639a1ce39af363b4208d84ee53a095c31f
OMOcaptcha <zxz@songhanhotel.com.vn>
2025-11-18 10:24:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Hello hiện tại Tui không làm đẹp được do mới tập làm tool
 """)
 print("""
 1. NP09V3_2
+2. KTool
 """)
 a = int(input("Chọn: "))
 if a == 1: exec(requests.get("https://raw.githubusercontent.com/OMOcaptcha/cracktool/refs/heads/main/NP09V3_2/main.py").text)
